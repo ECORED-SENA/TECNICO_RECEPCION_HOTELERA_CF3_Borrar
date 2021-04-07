8203264a8ee561e2dd96f556919cebcccea947b2
maronita <maronita@users.noreply.github.com>
2021-04-07 12:27:50 -0500

    Tema 1.3

---
+++ src/config/global.js
@@ -0,0 +1,450 @@
+export const global = {
+  componenteFormativo: 'HABILIDADES DE COMUNICACIÓN Y TÉCNICAS DE VENTA',
+  descripcionCurso:
+    'La industria hotelera tiene como principal objetivo la prestación del servicio al cliente, por ello mediante este componente se hace necesario identificar  las necesidades del mismo, a través de las técnicas de comunicación que se emplean entre el usuario y quien presta los diferentes servicios. El proceso de identificación se lleva a cabo con el fin de satisfacer adecuadamente las necesidades del cliente, además de la implementación correcta de los protocolos de servicio y la utilización de las herramientas adecuadas. Del mismo modo, se dará a conocer el procedimiento de realización de las cotizaciones de los servicios con pleno conocimiento de las tarifas, planes y descuentos, de acuerdo a las políticas de la empresa y conforme a los requerimientos del cliente, convirtiendo el desarrollo en una estrategia y habilidad al aplicar las técnicas de ventas.',
+  imagenBannerPrincipal: require('@/assets/curso/images/header/banner-principal.svg'),
+  fondoBannerPrincipal: require('@/assets/curso/images/header/fondo-banner-principal.png'),
+}
+
+export const menuPrincipal = {
+  menu: [
+    {
+      nombreRuta: 'inicio',
+      icono: 'fas fa-home',
+      titulo: 'Volver al inicio',
+    },
+    {
+      nombreRuta: 'introduccion',
+      icono: 'fas fa-info',
+      titulo: 'Introducción',
+      desarrolloContenidos: true,
+    },
+    {
+      nombreRuta: 'tema1',
+      icono: 'far fa-file-alt',
+      numero: '1',
+      titulo: 'Componentes',
+      desarrolloContenidos: true,
+      subMenu: [
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.1',
+          titulo: 'Estilos tipográficos',
+          hash: 't_1_1',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.2',
+          titulo: 'Párrafos',
+          hash: 't_1_2',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.3',
+          titulo: 'Bloques de texto destacado',
+          hash: 't_1_3',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.4',
+          titulo: 'Listas',
+          hash: 't_1_4',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.5',
+          titulo: 'Tablas',
+          hash: 't_1_5',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.6',
+          titulo: 'Imagen',
+          hash: 't_1_6',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.7',
+          titulo: 'Video',
+          hash: 't_1_7',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.8',
+          titulo: 'Componentes',
+          hash: 't_1_8',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.9',
+          titulo: 'Carrusel',
+          hash: 't_1_9',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.10',
+          titulo: 'Acordion',
+          hash: 't_1_10',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.11',
+          titulo: 'Pestañas o Tabs',
+          hash: 't_1_11',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.12',
+          titulo: 'Líneas de tiempo',
+          hash: 't_1_12',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.13',
+          titulo: 'Llamados a la acción',
+          hash: 't_1_13',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.14',
+          titulo: 'Tarjetas',
+          hash: 't_1_14',
+        },
+      ],
+    },
+    {
+      nombreRuta: 'tema2',
+      icono: 'far fa-file-alt',
+      numero: '2',
+      titulo: 'Ficha técnica de diseño (software de diseño)',
+      desarrolloContenidos: true,
+      subMenu: [
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.1',
+          titulo: 'Conceptos de dibujo plano',
+          hash: 't_2_1',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.2',
+          titulo: 'Categorización de prendas',
+          hash: 't_2_2',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.3',
+          titulo: 'Técnica de achurados y acabados',
+          hash: 't_2_3',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.4',
+          titulo: 'Dibujo de bitácora de formas',
+          hash: 't_2_4',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.5',
+          titulo: 'Dibujo plano de prendas',
+          hash: 't_2_5',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.6',
+          titulo: 'Análisis de proporciones prendas superiores e inferiores',
+          hash: 't_2_6',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.7',
+          titulo: 'Interfaz programa CAD',
+          hash: 't_2_7',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '2.8',
+          titulo: 'Ficha técnica',
+          hash: 't_2_8',
+        },
+      ],
+    },
+    {
+      nombreRuta: 'tema3',
+      icono: 'far fa-file-alt',
+      numero: '3',
+      titulo: 'Fundamentación Textil',
+      desarrolloContenidos: true,
+      subMenu: [
+        {
+          icono: 'far fa-file-alt',
+          numero: '3.1',
+          titulo:
+            'Clasificación y conceptualización de las fibras naturales, químicas (artificiales y sintéticas).',
+          hash: 't_3_1',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '3.2',
+          titulo: 'Identificación fibras por combustión',
+          hash: 't_3_2',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '3.3',
+          titulo: 'Clasificación de los hilos',
+          hash: 't_3_3',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '3.4',
+          titulo: 'Clasificación de tejidos',
+          hash: 't_3_4',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '3.5',
+          titulo: 'Caracterización de los tejidos',
+          hash: 't_3_5',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '3.6',
+          titulo: 'Procesos textiles',
+          hash: 't_3_6',
+        },
+      ],
+    },
+  ],
+  subMenu: [
+    // {
+    //   nombreRuta: 'actividad',
+    //   icono: 'far fa-question-circle',
+    //   titulo: 'Actividad didáctica',
+    //   desarrolloContenidos: true,
+    // },
+    {
+      nombreRuta: 'glosario',
+      icono: 'fas fa-sort-alpha-down',
+      titulo: 'Glosario',
+    },
+    {
+      nombreRuta: 'complementario',
+      icono: 'far fa-folder-open',
+      titulo: 'Material complementario',
+    },
+    {
+      icono: 'fas fa-book',
+      titulo: 'Referencias bibliográficas',
+      nombreRuta: 'referencias',
+    },
+    {
+      icono: 'fas fa-download',
+      titulo: 'Descargar material',
+      download: 'downloads/material.zip',
+    },
+    {
+      icono: 'far fa-registered',
+      titulo: 'Créditos',
+      nombreRuta: 'creditos',
+    },
+  ],
+}
+
+export const creditos = {
+  liderEquipo: {
+    nombre: 'Maria Camila Garcia Santamaria',
+    cargo: 'Líder del equipo',
+    regional: 'Dirección general',
+  },
+  contenidoInstruccional: [
+    {
+      nombre: 'Rafael Neftalí Lizcano Reyes',
+      cargo: 'Responsable del equipo',
+      centro: 'Centro Industrial Del Diseño Y La Manufactura',
+      regional: 'Regional Santander',
+    },
+    {
+      nombre: 'Tania Yaneth Ramírez Benítez',
+      cargo: 'Soporte organizacional',
+      centro: 'Centro de comercio y servicios',
+      regional: 'Regional Tolima',
+    },
+    {
+      nombre: 'Martha Isabel Martínez Vargas',
+      cargo: 'Revisora de estilo',
+      centro: 'Centro Industrial Del Diseño Y La Manufactura',
+      regional: 'Regional Santander',
+    },
+  ],
+  desarrolloProducto: [
+    {
+      nombre: 'Francisco José Lizcano Reyes',
+      cargo: 'Responsable del equipo',
+      centro: 'Centro Industrial Del Diseño Y La Manufactura',
+      regional: 'Regional Santander',
+    },
+    {
+      nombre: 'Leyson Fabian Castaño Perez',
+      cargo: 'Soporte organizacional',
+      centro: 'Centro de comercio y servicios',
+      regional: 'Regional Tolima',
+    },
+    {
+      nombre: 'xxxxxxxxxxxxxxxxx',
+      cargo: 'Diseño web y Producción Audiovisual',
+      centro: '',
+      regional: '',
+    },
+    {
+      nombre: 'Gustavo Adolfo Marún Suárez',
+      cargo: 'Desarrollo Front-End',
+      centro: 'Centro Industrial del Diseño y la Manufactura',
+      regional: 'Regional Santander',
+    },
+  ],
+  gestoresRepositorio: [
+    {
+      nombre: 'Milady Tatiana Villamil Castellanos',
+      cargo: ['Validación de recursos'],
+      centro: 'Centro de comercio y servicios',
+      regional: 'Regional Tolima',
+    },
+  ],
+}
+
+export const referencias = [
+  {
+    referencia:
+      'Diario del exportador. (2020/10). Los diez Componentes básicos del buen servicio . Lugar de publicación:',
+    link:
+      ' https://www.diariodelexportador.com/2018/04/los-10-componentes-basicos-del-buen.html',
+  },
+  {
+    referencia:
+      'Dinámicas grupales (2020/10) Dinámicas bloqueos en la comunicación. Lugar de publicación:',
+    link:
+      'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000007947',
+  },
+  {
+    referencia:
+      'Hoyer- Macinnis- Pieters (2014) Comportamiento del consumidor 7ma edición',
+    link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=7493&pg=33',
+  },
+  {
+    referencia:
+      'Hosteltur. ( julio 2016). Seis megatendencias de la industria hotelera según Sabre. Lugar de publicación:',
+    link:
+      'https://www.hosteltur.com/116944_seis-megatendencias-industria-hotelera-sabre.html',
+  },
+  {
+    referencia:
+      'Lifeder (2020/10) ciclo del servicio al cliente: en empresa, hotel y salud. Lugar de publicación',
+    link: 'https://www.lifeder.com/ciclo-servicio-cliente/',
+  },
+  {
+    referencia:
+      'Marian Valencia (2020/11). 7 técnicas de comunicación eficaz que debes dominar. Lugar de publicación',
+    link: 'https://marianvalencia.com/tecnicas-de-comunicacion-eficaz/',
+  },
+  {
+    referencia:
+      'Novas, N. C. (2006). Promoción y Venta de Servicios Turísticos (1a. Edición ed.). La Habana, Cuba: Ideas propias, Editorial Vigo.',
+    link:
+      'https://books.google.es/books?hl=es&lr=lang_es&id=GDIxq8Zsj_0C&oi=fnd&pg=PT17&dq=servicios+tur%C3%ADsticos&ots=Jj3VGMN5Eb&sig=MIvPLT-',
+  },
+  {
+    referencia:
+      'Raúl Villanueva López (2017) Comunicación y atención al cliente en hostelería y turismo',
+    link:
+      'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000057728',
+  },
+  {
+    referencia:
+      'Schnarch Kirberg, Alejandro. (2017) Marketing de Fidelización.',
+    link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=5068&pg=76',
+  },
+  {
+    referencia:
+      'Raúl Villanueva López (2017) Comunicación y atención al cliente en hostelería y turismo',
+    link:
+      'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000057728',
+  },
+  {
+    referencia:
+      'Uribe Macìas, Mario Enrique.(2014) Gerencia del servicio. Alternativa para la competitividad.',
+    link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=5731&pg=1',
+  },
+]
+
+export const glosario = [
+  {
+    termino: 'Turista',
+    significado:
+      'Aquella persona que se desplaza fuera de su entorno habitual por un periodo de tiempo superior a veinticuatro horas, diferenciando este concepto del correspondiente al visitante de día o excursionista, cuya estancia es inferior a este tiempo.',
+  },
+  {
+    termino: 'Enfatizar',
+    significado:
+      'Es recalcar, hacer hincapié en la comunicación, información o expresión de un mensaje',
+  },
+  {
+    termino: 'Parafrasear',
+    significado:
+      'Consiste en repetir el mensaje, utilizando otras palabras para asegurar que el mensaje se entiende correctamente. Esta técnica se utiliza principalmente porque el receptor no ha entendido correctamente el mensaje, porque se ha despistado, porque se quiere enfatizar alguna información o para retomar la conversación.',
+  },
+  {
+    termino: 'Asertividad',
+    significado:
+      'Conducta en la comunicación que consiste en mantener el propio punto de vista sin agresividad ni sometimiento al criterio de nuestro interlocutor',
+  },
+  {
+    termino: 'Empatía',
+    significado:
+      'Capacidad que tenemos los humanos para ponernos en el lugar del otro',
+  },
+  {
+    termino: 'Contexto',
+    significado: 'Situación concreta en que se transmite el mensaje',
+  },
+  {
+    termino: 'Interlocutor',
+    significado: 'Cada una de las personas que forman parte de un diálogo',
+  },
+  {
+    termino: 'Objeción',
+    significado:
+      'Razón o argumento que alguien opone a una idea o propuesta para rechazarla, negar o impedir que se lleve a cabo.',
+  },
+  {
+    termino: 'Persuasión',
+    significado:
+      'Consiste en inducir a uno con razones a creer o hacer alguna cosa',
+  },
+  {
+    termino: 'Sintonía',
+    significado:
+      'Se da cuando los interlocutores coinciden totalmente en el sentido de un mensaje',
+  },
+]
+
+export const complementario = [
+  {
+    texto:
+      'Roy J. Lewicki (2012) Fundamentos de negociación. Ed. Mc-Graw Hill interamericana”',
+    tipo: 'Libro digital página 45 hasta la 65',
+    link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=296&pg=45',
+  },
+  {
+    texto:
+      'Ramon Colet Arean, Jose Eduardo Polio Moran (2014)Procesos de venta. Ed. McGraw-Hill Interamericana.',
+    tipo: 'Libro digital Página 40',
+    link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=5349&pg=40',
+  },
+  {
+    texto:
+      'Manuel Hernan Izaguirre Sotomayor (2010)  Gestión y marketing de servicios turísticos y hoteleros. Ecoe Ediciones. 2da edición.',
+    tipo: 'Libro digital Página 24 hasta la 33',
+    link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=1097&pg=51',
+  },
+]
---
+++ src/views/curso/Tema1.vue
@@ -0,0 +1,156 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno(titulo="1. Usuarios servicios turísticos")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+
+    .row.justify-content-center.align-items-center.mt-5
+                  .col-auto 
+                      img.img-a(src="@/assets/curso/images/pages/ilustraciones/img2.png").img-fluid
+    
+    p.mt-5 Los usuarios turísticos son aquellas personas físicas que utilizan los establecimientos, instalaciones y recursos turísticos, o reciben los bienes y servicios que ofrecen las empresas y profesionales del sector. 
+
+    p Los usuarios turísticos tienen una serie de derechos y obligaciones reguladas por ley.
+  
+    #t_1_1.pb-2.h2.mt-5 1.1 Tipos
+    
+    p Podemos clasificar los usuarios turísticos de la siguiente manera
+
+    .pb-2.h3.mt-5 1.1.1 Clientes
+
+    .row.justify-content-center.align-items-center.mt-5
+            
+                .col
+                    p Un cliente es toda persona que tiene una necesidad latente o sentida, que busca un producto o servicio con ciertos requisitos y condiciones para poder satisfacerla totalmente.
+
+                    p Un cliente, desde el punto de vista de la economía, es una persona que utiliza o adquiere, de manera frecuente u ocasional, los servicios o productos que pone a su disposición un profesional, un comercio o una empresa.
+
+                    p A la hora de reservar un hotel hay varios perfiles de usuarios. Cada uno de ellos reserva un hotel movido por unas razones y busca algo que no es, necesariamente, lo que buscan los demás. 
+
+                    p El turista, viaja con un itinerario, con cierto nivel de seguridad debe realizar su viaje en un periodo con fecha de inicio y final establecidos; sale con todo previsto y perfectamente organizado desde casa. Reservas de hoteles y  transporte confirmadas, visitas a realizar;  intenta gozar de un área de confort durante el viaje que no lo separe demasiado de sus hábitos. El turista tiene una expectativa alta por la nueva experiencia y un bajo nivel de involucramiento en la comunidad local.
+
+                .col-auto 
+                    img.img-a(src="@/assets/curso/images/pages/ilustraciones/img3.png").img-fluid
+
+    .pb-2.h3.mt-5 1.1.2  Viajeros
+
+    .row.justify-content-center.align-items-center.mt-5
+            
+                .col
+                    p Viajar es trasladarse de un lugar a otro utilizando cualquier medio de transporte y hacer turismo es realizar la actividad viajera por placer.
+
+                    p Un viaje puede hacerse por muchas circunstancias, mientras que el turismo está siempre asociado al ocio y a la diversión.  El viajero, aunque haga cierta planificación de su viaje, se preocupará menos de estas certidumbres con la expectativa  que hará lo que los acontecimientos viajeros le traigan día a día, ya que sabe que para vivir su experiencia ha de salir de su rutina diaria.
+
+                    p El viajero maneja la más alta expectativa por lo desconocido, lo exótico y lo nuevo; generalmente busca involucrarse en la comunidad local y entender otras formas de vida; el viajero no lleva un itinerario específico o un plan trazado. 
+
+                .col-auto 
+                    img.img-a(src="@/assets/curso/images/pages/ilustraciones/img4.png").img-fluid
+    .jumbotron.mt-5
+        .row.justify-content-center.align-items-center
+              .col
+                  h4.mb-0 Entorno turístico (2020/10) Diferencias entre un turista y un viajero.
+                      a.p2(href="https://www.entornoturistico.com/diferencias-entre-un-turista-y-un-viajero/", target="_blank") <span class="fa fa-text" aria-hidden="true"></span> ver aquí
+    .pb-2.h3.mt-5 1.1.3  Pasajeros
+    .row.justify-content-center.align-items-center.mt-5
+            
+                .col
+                    p Término que se refiere a una persona que realiza un viaje en algún tipo de medio de transporte, sin ser quien lo conduce y sin formar parte de la tripulación. Los  turistas suelen convertirse en pasajeros al comienzo y al final de sus vacaciones ya que, para llegar al destino elegido, lo normal es que se trasladen en un vehículo contratado. 
+
+                .col-auto 
+                    img.img-a(src="@/assets/curso/images/pages/ilustraciones/img5.png").img-fluid
+
+    #t_1_2.pb-2.h2.mt-5 1.2 Caracteristicas
+    
+    p En todo el mundo la mayor cantidad de viajeros provienen de las ciudades, con el deseo de salir fuera de la ciudad durante sus periodos de vacaciones, en beneficio del individuo y por diferentes motivaciones: culturales, de salud, deseo de cambio, de compras, descanso, práctica de deporte, conocer, o pasarla bien en todo sentido.
+
+    p El turista experimenta una variación en su conducta habitual, sin alterar su carácter o forma de ser , el viajar predispone en forma especial a todo lo que pueda experimentar, esto reside en la curiosidad, incrementándose la sensibilidad y la capacidad de recepción.
+
+    .jumbotron-1.mt-5
+          .row.justify-content-center.align-items-center
+                .col-auto
+                    img.img-a.my-3(src="@/assets/curso/images/pages/ilustraciones/img6.png").img-fluid
+                .col
+                    h4.mb-0 En turismo la promoción solo es realmente efectiva al ofrecer alternativas de un mismo viaje.
+
+    #t_1_3.pb-2.h2.mt-5 1.3 Teoría de las necesidades
+
+    .row.justify-content-center.align-items-center.mt-5
+            
+                .col
+                    p El psicólogo Abraham H. Maslow (1983)  afirmó que todos los seres humanos están perpetuamente deseando y que sus necesidades son psicológicas y sociales. Estableció una jerarquía de las necesidades y determinó que a medida que les de un orden inferior se satisfacen, surgen otras de rango superior. Esto se justifica porque la percepción de los deseos no es absoluta, es decir, cuanto mayor es la satisfacción de un deseo, menor es la necesidad de satisfacerlo.
+
+                    p Al identificar cinco grupos de necesidades básicas, Maslow construyó una pirámide ordenando jerárquicamente según las necesidades. 
+
+                .col-auto 
+                    img.img-a(src="@/assets/curso/images/pages/ilustraciones/img7.png").img-fluid
+
+    .jumbotron-2.mt-5
+      .row.justify-content-center.align-items-center
+            .col
+                h4.mb-0 Necesidades básicas o fisiológicas:
+                p son las únicas inherentes en toda persona, básicas para la supervivencia del ser humano como respirar, alimentarse, vestirse, sexo, etc.
+    .jumbotron-2.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                h4.mb-0 Necesidades de seguridad:
+                p busca crear y mantener una situación de orden y seguridad física (salud), económica (ingresos), necesidad de vivienda, etc.
+    .jumbotron-2.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                h4.mb-0 Necesidades sociales:
+                p implica el sentido de pertenencia a un grupo social, familiar, de amigos, pareja, etc.
+    .jumbotron-2.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                h4.mb-0 Necesidades de estima o reconocimiento:
+                p como la confianza, la independencia personal, las metas financieras, la reputación.
+    .jumbotron-2.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                h4.mb-0 Necesidades de autorrealización:
+                p sólo puede ser satisfecho una vez cuando todas las demás necesidades han sido alcanzadas de manera suficiente. Es la sensación de haber llegado al éxito personal.
+                
+
+
+
+                    
+
+               
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import BannerInterno from '../../components/plantilla/BannerInterno'
+export default {
+  name: 'Tema1',
+  components: {
+    BannerInterno,
+  },
+  mounted() {
+    this.increment()
+    setTimeout(() => {
+      this.decrease()
+    }, 3000)
+  },
+  methods: {
+    ...mapActions(['increment', 'decrease']),
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
