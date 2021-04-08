82e72a6a65b4a356b6389cc6804e6af016ffeb87
maronita <maronita@users.noreply.github.com>
2021-04-07 19:40:40 -0500

    Tema 2.1

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -25,9 +25,9 @@
 
                     p Un cliente, desde el punto de vista de la economía, es una persona que utiliza o adquiere, de manera frecuente u ocasional, los servicios o productos que pone a su disposición un profesional, un comercio o una empresa.
 
-                    p A la hora de reservar un hotel hay varios perfiles de usuarios. Cada uno de ellos reserva un hotel movido por unas razones y busca algo que no es, necesariamente, lo que buscan los demás. 
+                    p A la hora de reservar un hotel hay varios perfiles de usuarios. <b>Cada uno de ellos reserva un hotel movido por unas razones y busca algo que no es, necesariamente</b>, lo que buscan los demás. 
 
-                    p El turista, viaja con un itinerario, con cierto nivel de seguridad debe realizar su viaje en un periodo con fecha de inicio y final establecidos; sale con todo previsto y perfectamente organizado desde casa. Reservas de hoteles y  transporte confirmadas, visitas a realizar;  intenta gozar de un área de confort durante el viaje que no lo separe demasiado de sus hábitos. El turista tiene una expectativa alta por la nueva experiencia y un bajo nivel de involucramiento en la comunidad local.
+                    p El turista, viaja con un itinerario, con cierto nivel de seguridad debe realizar su viaje en un periodo con fecha de inicio y final establecidos; <b>sale con todo previsto y perfectamente organizado desde casa. Reservas de hoteles y  transporte confirmadas, visitas a realizar</b>;  intenta gozar de un área de confort durante el viaje que no lo separe demasiado de sus hábitos. El turista tiene una expectativa alta por la nueva experiencia y un bajo nivel de involucramiento en la comunidad local.
 
                 .col-auto 
                     img.img-a(src="@/assets/curso/images/pages/ilustraciones/img3.png").img-fluid
@@ -61,9 +61,9 @@
 
     #t_1_2.pb-2.h2.mt-5 1.2 Caracteristicas
     
-    p En todo el mundo la mayor cantidad de viajeros provienen de las ciudades, con el deseo de salir fuera de la ciudad durante sus periodos de vacaciones, en beneficio del individuo y por diferentes motivaciones: culturales, de salud, deseo de cambio, de compras, descanso, práctica de deporte, conocer, o pasarla bien en todo sentido.
+    p En todo el mundo la mayor cantidad de viajeros provienen de las ciudades, con el deseo de salir fuera de la ciudad durante sus periodos de vacaciones, <b> en beneficio del individuo y por diferentes motivaciones: culturales, de salud, deseo de cambio, de compras, descanso, práctica de deporte, conocer, o pasarla bien en todo sentido.</b>
 
-    p El turista experimenta una variación en su conducta habitual, sin alterar su carácter o forma de ser , el viajar predispone en forma especial a todo lo que pueda experimentar, esto reside en la curiosidad, incrementándose la sensibilidad y la capacidad de recepción.
+    p El turista experimenta una variación en su conducta habitual, sin alterar su carácter o forma de ser , el viajar predispone en forma especial a todo lo que pueda experimentar, <b>esto reside en la curiosidad</b>, incrementándose la sensibilidad y la capacidad de recepción.
 
     .jumbotron-1.mt-5
           .row.justify-content-center.align-items-center
@@ -121,11 +121,11 @@
             p.text-center Necesidad, deseo y la demanda turística. 
         .col
 
-          p Ya entendemos a que se refiere “la necesidad”, el deseo se conoce como la forma en que una persona expresa la manera de satisfacer una necesidad en la que inciden varios factores como las características personales, su educación y creencias, entorno cultural social y ambiental, los estímulos de marketing y su capacidad adquisitiva.
+          p Ya entendemos a que se refiere <b>“la necesidad”</b>, el deseo se conoce como la forma en que una persona expresa la manera de satisfacer una necesidad en la que inciden varios factores como las características personales, su educación y creencias, entorno cultural social y ambiental, los estímulos de marketing y su capacidad adquisitiva.
 
           p Un deseo se convierte en demanda cuando una persona, canaliza el deseo para realizar una petición concreta de un producto.
 
-          p La demanda turística es el conjunto de bienes y servicios que los turistas están dispuesto a adquirir en un determinado destino, incluye el consumo y la experiencia de lugares, su motivación principal recae en el destino turístico, entre los factores que influyen en esta demanda son el poder económico, disponibilidad de tiempo y otros factores que le motiven.
+          p La demanda turística <b>es el conjunto de bienes y servicios que los turistas están dispuesto a adquirir en un determinado destino, incluye el consumo y la experiencia de lugares</b>, su motivación principal recae en el destino turístico, entre los factores que influyen en esta demanda son el poder económico, disponibilidad de tiempo y otros factores que le motiven.
 
         .col-auto.mt-5 
                     img.img-a(src="@/assets/curso/images/pages/ilustraciones/img8.png").img-fluid
@@ -138,31 +138,137 @@
 
         p.mt-3 El personal en los alojamientos siempre está dispuesto a enfrentar determinadas situaciones especiales en las cuales sus decisiones son la clave para resolver eficientemente los problemas generados.
 
-    .row.justify-content-center.mb-5
-        .col-6.col-md-6 
-          .col-6
-            img(src="@/assets/curso/images/pages/ilustraciones/ico1.png", alt="", style="width:60px; display:block; margin:auto 0;")
-              p.text-center Algunas posibles situaciones, entre otras.
-          .col-6
-            img(src="@/assets/curso/images/pages/ilustraciones/ico2.png", alt="", style="width:60px; display:block; margin:auto 0;")
-              p.text-center Trato personalizado con el huésped.
-          .col-6
-            img(src="@/assets/curso/images/pages/ilustraciones/ico3.png", alt="", style="width:60px; display:block; margin:auto 0;")
-              p.text-center Flexibilidad en los horarios de entrada y salida.    
-          .col-6
-            img(src="@/assets/curso/images/pages/ilustraciones/ico4.png", alt="", style="width:60px; display:block; margin:auto 0;")
-              p.text-center Ubicación del establecimiento y fácil reserva.
-          .col-6
-            img(src="@/assets/curso/images/pages/ilustraciones/ico5.png", alt="", style="width:60px; display:block; margin:auto 0;")
-              p.text-center Salas de juntas para optimizar las reuniones de negocios.
-          .col-6
-            img(src="@/assets/curso/images/pages/ilustraciones/ico6.png", alt="", style="width:60px; display:block; margin:auto 0;")
-              p.text-center Conectividad inalámbrica o wifi.
+        .i-general-1
+          div.div-gradient-2
+            .div.c-card-2
+              img.img-a(src='@/assets/curso/images/pages/ilustraciones/ico1.png' alt="",style="width:100px; display:block; margin:auto 0;").img-float
+              p.mt-4.p2 Algunas posibles situaciones, entre otras.
+          div.div-gradient-2 
+            .div.c-card-2
+              img.img-a(src='@/assets/curso/images/pages/ilustraciones/ico2.png' alt="", style="width:100px; display:block; margin:auto 0;").img-float 
+              p.mt-5.p2 Trato personalizado con el huésped
+          div.div-gradient-2
+            .div.c-card-2
+              img.img-a(src='@/assets/curso/images/pages/ilustraciones/ico3.png' alt="", style="width:100px; display:block; margin:auto 0;").img-float 
+              p.mt-3.p2 Flexibilidad en los horarios de entrada y salida.
+          div.div-gradient-2
+            .div.c-card-2
+              img.img-a(src='@/assets/curso/images/pages/ilustraciones/ico4.png' alt="", style="width:100px; display:block; margin:auto 0;").img-float 
+              p.mt-4.p2 Ubicación del establecimiento y fácil reserva.
+          div.div-gradient-2
+            .div.c-card-2
+              img.img-a(src='@/assets/curso/images/pages/ilustraciones/ico5.png' alt="", style="width:100px; display:block; margin:auto 0;").img-float 
+              p.mt-3.p2 Salas de juntas para optimizar las reuniones de negocios.
+          div.div-gradient-2
+            .div.c-card-2
+              img.img-a(src='@/assets/curso/images/pages/ilustraciones/ico6.png' alt="", style="width:100px; display:block; margin:auto 0;").img-float
+              p.mt-3.p1 Conectividad inalámbrica o wifi.
+        
+        p.mt-2 El establecimiento siempre estará en disposición de cumplir los requerimientos pero dentro de medidas y políticas establecidas que permitan al huésped recibir lo que requiere y que al mismo tiempo no implique inversiones extraordinarias.
+
+    .row.justify-content-center.align-items-center.mt-5
+
+
+    #t_1_5.pb-2.h2.mt-5 1.5 Motivaciones de compra
+
+    .row.justify-content-center.align-items-center.mt-5
+            
+                .col
+                    p p Los consumidores se ven influidos por una serie de factores y de condicionantes a la hora de elegir su destino para hacer turismo.  
+
+                    p La demanda se ve afectada por los valores del individuo y su clase social. La mentalidad y el comportamiento van a marcar y definir los productos turísticos: <b> el placer de viajar, los negocios, las condiciones de vida, el tiempo de ocio, los recursos financieros, el acceso a nuevas tecnologías, </b>etc. 
+
+                    p El producto turístico enmarca un espacio de satisfacción de necesidades, <b> los turistas llegan con una serie de expectativas sobre lo que pueden hacer por ellos </b> (Ford 2001, por otro lado, el turista juzga los servicios a través de lo que percibe y cómo percibe.
+
+                .col-auto 
+                    img.img-a(src="@/assets/curso/images/pages/ilustraciones/img9.png").img-fluid     
+    
+    #t_1_6.pb-2.h2.mt-5 1.6 Hábitos y tendencias de consumo
+                .col-auto 
+                    img.mt-5.img-a(src="@/assets/curso/images/pages/ilustraciones/img10.png").img-fluid
+                    p.mt-5 El objetivo final de la industria hotelera tiene que ser conocer las expectativas y necesidades de cada turista y buscar mecanismos que permitan cubrirlas e intentar superarlas.
+
+                    p.mt-2 El problema al que se enfrentan los hoteles, es que estas expectativas de los clientes van evolucionando, por lo cual tienen que analizar constantemente las tendencias y nuevas necesidades.  Para esto hay que realizar un análisis del comportamiento de los clientes, no como turistas sino como consumidores, esto permite identificar sus patrones de comportamiento y permitirá definir qué productos y experiencias les satisfacen.
+
+                    p.mt-2 Cuanto más se conozca el cliente más fácil será:
+
+                    p.mt-2 > Cubrir sus expectativas o deseos, intentado superarlos.
+                    p > Ofrecerle productos que se adapten a su perfil y aumentar su gasto dentro de la industria turística.
+                    p > Fidelizarlo a la empresa.
+
+                    p.mt-4 Seis megatendencias en la industria hotelera, como son las demandas del turista
+
+    .jumbotron-3.mt-5
+      .row.justify-content-center.align-items-center
+            .col
+                img.number(src='@/assets/curso/images/pages/ilustraciones/1.svg' alt="", style="width:30px; display:block; margin:auto 0;").img-float
+                p.mt-4.text-right Si la reserva se confirma, se marca la casilla correspondiente (OK).
+    .jumbotron-3.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                img(src='@/assets/curso/images/pages/ilustraciones/2.svg' alt="", style="width:30px; display:block; margin:auto 0;").img-float
+                p.mt-2.text-right La experiencia recibiendo el servicio en áreas como reservas, recepción y dentro del mismo hotel sea sencilla, cómoda y rápida. También se puede hacer uso de la geolocalización para enviarles información o notificaciones sobre productos basados en el lugar donde se encuentre el cliente.
+    .jumbotron-3.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                img(src='@/assets/curso/images/pages/ilustraciones/3.svg' alt="", style="width:30px; display:block; margin:auto 0;").img-float
+                p.mt-3.text-right Los consumidores buscan marcas que se identifiquen con ellos de una manera emocional, que genere sentimientos con cualidades que les generen seguridad.  El toque personal ayuda al establecimiento a posicionarse.
+    .jumbotron-3.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                img(src='@/assets/curso/images/pages/ilustraciones/4.svg' alt="", style="width:30px; display:block; margin:auto 0;").img-float
+                p.mt-2.text-right La tecnología se ha vuelto universal y afecta directamente la hotelería en diversas formas, los turistas se sienten muy bien con la variedad que esta le ofrece como información instantánea, herramientas de compra, canales de venta, opciones de productos y variedad de precios.
+    .jumbotron-3.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                img(src='@/assets/curso/images/pages/ilustraciones/5.svg' alt="", style="width:30px; display:block; margin:auto 0;").img-float
+                p.mt-3.text-right Los hoteles tienen que ser creativos con las políticas de precios, no necesariamente bajando los precios sino ofreciendo valor añadido.
+    .jumbotron-3.mt-3
+      .row.justify-content-center.align-items-center
+            .col
+                img(src='@/assets/curso/images/pages/ilustraciones/6.svg' alt="", style="width:30px; display:block; margin:auto 0;").img-float
+                p.mt-3.text-right No se puede centrar en ofrecer un producto por las características específicas del cliente como su nivel cultural entre otros, sino enfocarlo a sus deseos y motivaciones concretas.      
+
+    .jumbotron.mt-5
+        .row.justify-content-center.align-items-center
+              .col
+                  h4.mb-0 Sabre publica en el siguiente enlace 
+                      a.p2(href="https://www.hosteltur.com/116944_seis-megatendencias-industria-hotelera-sabre.html/", target="_blank") <span class="fa fa-text" aria-hidden="true"></span> ver aquí
+
+    #t_1_7.pb-2.h2.mt-5 1.7 Incidencia de la cultura en el comportamiento
+
+    p.mt-3 En gran parte, las decisiones de consumo y la forma de procesar la información se ven afectadas por <b>la cultura. La cultura se refiere a las conductas típicas o esperadas, las normas e ideas que caracterizan a un grupo de personas;</b> puede ser una poderosa influencia en todos los aspectos del comportamiento humano. 
+
+    .row.justify-content-center.align-items-center.mt-5
+            
+                .col
+                    p Cuando se observan grupos que se perciben similares a sus propios gustos se les considera como grupos de referencia, es decir, personas cuyos valores y opiniones comparten y respetan. #[br]
+
+                    p Su edad, su género y  formación académica pueden afectar, ejemplo, en lo que pueden ser unas buenas vacaciones.  Otros factores como la clase social y composición familiar, aun sus valores, personalidad y estilo de vida tiene un efecto en la toma de decisiones en el momento de realizar turismo o utilizar los establecimientos de servicios hoteleros.
+
+                .col-auto 
+                    img.img-a(src="@/assets/curso/images/pages/ilustraciones/img11.png").img-fluid 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -1,31 +1,50 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno
+  BannerInterno(titulo="2. Servicio al cliente")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
-    #t_2_1.pb-5.h2 tema 2.1
-    .pb-5(v-for="item of 15")
+  
+      .row.justify-content-center.align-items-center.mt-5
+                  .col-auto 
+                      img.img-a(src="@/assets/curso/images/pages/ilustraciones/img12.png").img-fluid
 
-    #t_2_2.pb-5.h2 tema 2.2
-    .pb-5(v-for="item of 15")
+      #t_2_1.pb-1.h2.mt-5 2.1 Concepto
 
-    #t_2_3.pb-5.h2 tema 2.3
-    .pb-5(v-for="item of 15")
+      .row.justify-content-center.align-items-center.mt-5 
+        .col
 
-    #t_2_4.pb-5.h2 tema 2.4
-    .pb-5(v-for="item of 15")
+          p Se entiende por servicio al cliente el conjunto de acciones, procesos y ejecuciones que el cliente espera, además del producto básico, como consecuencia del precio y la imagen. Son las actividades que buscan satisfacer las necesidades, deseos y expectativas  de un cliente.
 
-    #t_2_5.pb-5.h2 tema 2.5
-    .pb-5(v-for="item of 15")
+          p En consecuencia, el objetivo institucional no es solo vender, sino ganar y mantener clientes satisfechos.
 
-    #t_2_6.pb-5.h2 tema 2.6
-    .pb-5(v-for="item of 15")
+          p La experta en calidad del servicio Lisa Ford (2013) dice:
 
-    #t_2_7.pb-5.h2 tema 2.7
-    .pb-5(v-for="item of 15")
+        .col
+          .div.div-gradient-2
+            .div.c-card-
+          
+            p.text-center “si no se cuida lo básico, de nada servirán los detalles y los extras”.
+            
+      
+      #t_2_2.pb-5.h2 tema 2.2
+      .pb-5(v-for="item of 15")
 
-    #t_2_8.pb-5.h2 tema 2.8
-    .pb-5(v-for="item of 15")
+      #t_2_3.pb-5.h2 tema 2.3
+      .pb-5(v-for="item of 15")
+
+      #t_2_4.pb-5.h2 tema 2.4
+      .pb-5(v-for="item of 15")
+
+      #t_2_5.pb-5.h2 tema 2.5
+      .pb-5(v-for="item of 15")
+
+      #t_2_6.pb-5.h2 tema 2.6
+      .pb-5(v-for="item of 15")
+
+      #t_2_7.pb-5.h2 tema 2.7
+      .pb-5(v-for="item of 15")
+
+      #t_2_8.pb-5.h2 tema 2.8
+      .pb-5(v-for="item of 15")
 
 </template>
 
